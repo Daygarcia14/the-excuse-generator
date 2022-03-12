bbcd10e2bca061ee9d6a02cb8218a89017fee80f
Dayanna García <dayanna_garcia@hotmail.com>
2022-03-11 23:37:14 -0400

    Add repository excuse-generator

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function (){
+    let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['eat','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+    
+    let excuseAleatory = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + 
+    when[Math.floor(Math.random() * when.length)];
+    
+    document.getElementById("excuse").innerHTML = excuseAleatory;
+    
+    }
